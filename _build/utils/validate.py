31c1fc2204a846b747fe92347f45c11631785cee
r0bespierre <mar299@georgetown.edu>
2017-02-14 10:15:17 -0500

    Updated validation script with various warning statements

--- _build/utils/validate.py
+++ _build/utils/validate.py
@@ -1,10 +1,12 @@
+# -*- coding: utf-8 -*-
+
 import os, ntpath, sys
 import shutil
 from glob import glob
 import re
+from collections import defaultdict
 import xml.etree.ElementTree as ET
 
-#coding: utf-8
 
 # Function to validate list of XML files against XSD schema
 def validate_xsd(file_list, gum_source):
@@ -17,8 +19,8 @@ def validate_xsd(file_list, gum_source):
 	schema = etree.XMLSchema(schema_root)
 
 	for docnum, xml_file in enumerate(file_list):
-		print "\t+ " + " "*40 + "\r",
-		print " " + str(docnum+1) + "/" + str(len(file_list)) + ":\t+ Validating " + xml_file + "\r",
+		print "\t+ " + " " * 40 + "\r",
+		print " " + str(docnum + 1) + "/" + str(len(file_list)) + ":\t+ Validating " + xml_file + "\r",
 
 		try:
 			with open(gum_source + "xml" + os.sep + xml_file, 'r') as f:
@@ -30,12 +32,12 @@ def validate_xsd(file_list, gum_source):
 			else:
 				errors += "\n" + xml_file + " has errors:\n"
 				for err in schema.error_log.filter_from_errors():
-					err_match = re.search(r'\.xml:([0-9]+).*?: (.*)',str(err))
+					err_match = re.search(r'\.xml:([0-9]+).*?: (.*)', str(err))
 					if err_match is not None:
 						errors += "  Line " + err_match.group(1) + ": " + err_match.group(2) + "\n"
 					else:
-						errors += "\n  "+err + "\n"
-	print "o " + str(valid_files) + " documents pass XSD validation" + " "*30
+						errors += "\n  " + err + "\n"
+	print "o " + str(valid_files) + " documents pass XSD validation" + " " * 30
 	if len(errors) > 0:
 		print errors
 		print "Aborting due to validation errors"
@@ -47,14 +49,14 @@ def count_tokens(e):
 	tok_count = 0
 	lines = e.text.split('\n') + e.tail.split('\n')
 	for line in lines:
-		if line.count('\t') > 0	:
+		if line.count('\t') > 0:
 			tok_count += 1
 	for child in e:
 		tok_count += count_tokens(child)
 	return tok_count
 
-def validate_src(gum_source):
 
+def validate_src(gum_source):
 	dirs = [('xml', 'xml'), ('dep', 'conll10'), ('rst', 'rs3'), ('tsv', 'tsv')]
 
 	# check that each dir has same # and names of files (except extensions)
@@ -71,25 +73,25 @@ def validate_src(gum_source):
 			else:
 				filenames.append(filename_validate.group(1))
 
-		print "Found "+ str(len(filenames)) +" in "+ dir_ext + "\r",
+		print "Found " + str(len(filenames)) + " in " + dir_ext + "\r",
 		file_lists.append(filenames)
-	
+
 	# check that filenames are the same across dirs
-	if all(len(x)==len(file_lists[0]) for x in file_lists) is False:
+	if all(len(x) == len(file_lists[0]) for x in file_lists) is False:
 		print 'x Different numbers of files in directories:'
 		for d in xrange(len(dirs)):
 			print str(dirs[d][0]) + ": " + str(len(file_lists[d]))
 		exit()
 	else:
 		for i in xrange(len(file_lists[0])):
-			same_names = all(x[i]==file_lists[0][i] for x in file_lists)
+			same_names = all(x[i] == file_lists[0][i] for x in file_lists)
 			if same_names is False:
 				print 'Different filenames:'
 				for d in xrange(len(dirs)):
 					print str(dirs[d][0]) + ": " + file_lists[d][i]
 	print "o Found " + str(len(file_lists[0])) + " documents"
 	print "o File names match"
-	
+
 	# check # of tokens
 	print "Checking identical token counts...\r",
 	all_tok_counts = []
@@ -98,24 +100,24 @@ def validate_src(gum_source):
 		filenames = file_lists[d]
 		for filename in filenames:
 			filepath = gum_source + str(dirs[d][0]) + os.sep + filename + "." + dirs[d][1]
-			#filename = os.path.abspath(filepath)
+			# filename = os.path.abspath(filepath)
 			with open(filepath) as this_file:
 				file_lines = this_file.readlines()
-	
+
 				if dirs[d][0] == 'xml':
 					tok_count = 0
 					for line in file_lines:
 						if line.count('\t') > 0:
 							tok_count += 1
 					dir_tok_counts.append(tok_count)
-	
+
 				elif dirs[d][0] == 'dep':
 					tok_count = 0
 					for line in file_lines:
 						if line.count('\t') == 9:
 							tok_count += 1
 					dir_tok_counts.append(tok_count)
-	
+
 				# rst -- use xml reader, add up space-split counts of segment.text
 				elif dirs[d][0] == 'rst':
 					tok_count = 0
@@ -127,20 +129,19 @@ def validate_src(gum_source):
 						seg_tok_count = len(segment.text.strip().split(" "))
 						tok_count += seg_tok_count
 					dir_tok_counts.append(tok_count)
-	
+
 				elif dirs[d][0] == 'tsv':
 					tok_count = 0
 					for line in file_lines:
 						if line.count('\t') > 0:
 							tok_count += 1
 					dir_tok_counts.append(tok_count)
-	
-	
+
 		all_tok_counts.append(dir_tok_counts)
 
 	token_counts_match = True
 	for i in xrange(len(all_tok_counts[0])):
-		same_count = all(x[i]==all_tok_counts[0][i] for x in all_tok_counts)
+		same_count = all(x[i] == all_tok_counts[0][i] for x in all_tok_counts)
 		if same_count is False:
 			print "x Different token counts in " + file_lists[0][i] + ":"
 			for d in xrange(len(all_tok_counts)):
@@ -148,11 +149,11 @@ def validate_src(gum_source):
 				token_counts_match = False
 	if token_counts_match:
 		print "o Token counts match across directories"
-	
+
 	# check sentences (based on tok count)
 	all_sent_lengths = []
-	sentence_dirs = [('xml', 'xml'), ('dep', 'conll10')] # just the dirs where we check sentences
-	
+	sentence_dirs = [('xml', 'xml'), ('dep', 'conll10')]  # just the dirs where we check sentences
+
 	for d in xrange(len(sentence_dirs)):
 		dir_sent_lengths = []
 		filenames = file_lists[d]
@@ -160,15 +161,15 @@ def validate_src(gum_source):
 			filepath = str(sentence_dirs[d][0]) + os.sep + filename + "." + sentence_dirs[d][1]
 			file_sent_lengths = []
 			with open(gum_source + filepath) as this_file:
-	
+
 				if sentence_dirs[d][0] == 'xml':
 					tree = ET.parse(gum_source + filepath)
 					root = tree.getroot()
 					for s in root.iter('s'):
 						sent_length = count_tokens(s)
-	
+
 						file_sent_lengths.append(sent_length)
-	
+
 				elif sentence_dirs[d][0] == 'dep':
 					file_text = this_file.read().strip() + "\n\n"
 					sentences = file_text.split('\n\n')
@@ -179,10 +180,10 @@ def validate_src(gum_source):
 							if line.count('\t') == 9:
 								sent_length += 1
 						file_sent_lengths.append(sent_length)
-	
+
 			dir_sent_lengths.append(file_sent_lengths)
 		all_sent_lengths.append(dir_sent_lengths)
-	
+
 	for i in xrange(len(all_sent_lengths[0])):
 		same_lengths = all(x[i] == all_sent_lengths[0][i] for x in all_sent_lengths)
 		if not same_lengths:
@@ -199,6 +200,7 @@ def validate_src(gum_source):
 		print "i WARN: module lxml is not installed"
 		print "i Skipping XSD validation of XML files"
 		print "i (to fix this warning: pip install lxml)"
+
 
 def validate_annos(gum_source):
 	xml_source = gum_source + "xml" + os.sep
@@ -217,7 +219,11 @@ def validate_annos(gum_source):
 		funcs = {}
 		tokens = {}
 		parent_ids = {}
+		lemmas = {}
+		sent_types = {}
 		parents = {}
+		children = defaultdict(list)
+		child_funcs = defaultdict(list)
 		tok_num = 0
 
 		depfile = xmlfile.replace("xml" + os.sep, "dep" + os.sep).replace("xml", "conll10")
@@ -234,12 +240,17 @@ def validate_annos(gum_source):
 					tok_num += 1
 					fields = line.split("\t")
 					funcs[tok_num] = fields[7]
-					parent_ids[tok_num] = int(fields[6]) + sent_start if fields[6] != "0" else 0
+					if fields[6] != "0":  # Root token
+						parent_ids[tok_num] = int(fields[6]) + sent_start
+						children[int(fields[6]) + sent_start].append(fields[1])
+						child_funcs[int(fields[6]) + sent_start].append(fields[7])
+					else:
+						parent_ids[tok_num] = 0
 					tokens[tok_num] = fields[1]
 			elif len(line) == 0:
 				sent_start = tok_num
 
-		for i in xrange(1,len(tokens)+1,1):
+		for i in xrange(1, len(tokens) + 1, 1):
 			if parent_ids[i] == 0:
 				parents[i] = "ROOT"
 			else:
@@ -248,39 +259,46 @@ def validate_annos(gum_source):
 		xml_lines = open(xmlfile).read().replace("\r", "").split("\n")
 		tok_num = 0
 
+		s_type = ""
+		for line in xml_lines:
+			if "\t" in line:  # Token
+				tok_num += 1
+				lemmas[tok_num] = line.split("\t")[2]
+				sent_types[tok_num] = s_type
+			else:
+				m = re.search(r's type="([^"]+)"', line)
+				if m is not None:
+					s_type = m.group(1)
+
+		tok_num = 0
+
 		for line in xml_lines:
 			if "\t" in line:  # Token
 				tok_num += 1
 				func = funcs[tok_num]
 				fields = line.split("\t")
-				if len(fields) < 3:
-					print docname
 				tok, pos, lemma = fields[0:3]
 				parent_string = parents[tok_num]
 				parent_id = parent_ids[tok_num]
-				flag_dep_warnings(tok_num, tok, pos, lemma, func, parent_string, parent_id, docname)
+				parent_lemma = lemmas[parent_ids[tok_num]] if parent_ids[tok_num] != 0 else ""
+				flag_dep_warnings(tok_num, tok, pos, lemma, func, parent_string, parent_lemma, parent_id,
+								  children[tok_num], child_funcs[tok_num], sent_types[tok_num], docname)
 
-def flag_dep_warnings(id, tok, pos, lemma, func, parent, parent_id, docname):
+
+def flag_dep_warnings(id, tok, pos, lemma, func, parent, parent_lemma, parent_id, children, child_funcs, s_type,
+					  docname):
 	# Shorthand for printing errors
 	inname = " in " + docname + " @ token " + str(id) + " (" + tok + " <- " + parent + ")"
 
-	# Uses an array to look through VB/VH verb tenses
-	temp_pos = ["", "D", "G", "N", "P", "Z"]
-	for pos_letter in temp_pos:
-		#if pos == "VB" + pos_letter and lemma != "be":		I turned these off in favor of the regex versions.
-		#	print "WARN: VB.* must be 'be'" + inname		They are otherwise identical.
-		#if pos == "VH" + pos_letter and lemma != "have":
-		#	print "WARN: VH.* must be 'have'" + inname
-
-		#I couldn't figure out how to make this one w/o an array
-		if re.search(r"have|^be$", lemma, re.IGNORECASE) is not None and pos == "VV" + pos_letter:
-			print "WARN: VV.* may not be 'be' or 'have' & not " + lemma + inname
-
 	# This is an alternate of the array version, using regex instead of arrays
-	if re.search(r"VH.*", pos, re.IGNORECASE) is not None and lemma != "have":
+	if re.search(r"VH.*", pos) is not None and lemma != "have":
 		print "WARN: VH.* must be 'have' & not lemma " + lemma + inname
-	if re.search(r"VB.*", pos, re.IGNORECASE) is not None and lemma != "be":
+	if re.search(r"VB.*", pos) is not None and lemma != "be":
 		print "WARN: VB.* must be 'be' & not lemma " + lemma + inname
+	if re.search(r"VV.*", pos) is not None and lemma == "be":
+		print "WARN: VV.* must not be 'be'" + inname
+	if re.search(r"VV.*", pos) is not None and lemma == "have":
+		print "WARN: VV.* must not be 'have'" + inname
 
 	# Finds mwe expressions that are pointing backwards.
 	if func == 'mwe' and id < parent_id:
@@ -291,7 +309,7 @@ def flag_dep_warnings(id, tok, pos, lemma, func, parent, parent_id, docname):
 		print "WARN: auxpass must be 'be' or 'get'" + inname
 
 	# Finds mistagged negs, but does not take non-ASCII apostrophes.
-	if re.search(r"never|not|no|n't", tok, re.IGNORECASE) is None and func == "neg":
+	if re.search(r"never|not|no|n't|n’t", tok, re.IGNORECASE) is None and func == "neg":
 		print "WARN: mistagged neg " + tok + inname
 
 		# None of what follows works.
@@ -301,20 +319,14 @@ def flag_dep_warnings(id, tok, pos, lemma, func, parent, parent_id, docname):
 		# else:
 		#	print "This is a " + u"\u2019" + inname
 
-	# Finds tokens with the lemma "be" in which the function is not listed in temp_func
-	temp_func = ["cop", "aux", "root", "csubj", "auxpass"]		# I'm certain this can be done more efficiently.
-	temp_var = 0												# Also, this returns a loooooooot of results.
-	if temp_var < 5 :											# Should it?
-		for temp_dep in temp_func:
-			if lemma == "be" and func == temp_dep:	# If the dep is correct , then the loop ends.
-				temp_var = temp_var + 6
-			if lemma == "be" and func != temp_dep:
-				temp_var = temp_var + 1				# The statement increases one time for each possible dep.
-	if temp_var == 5:								# If the dep is ultimately incorrect, the print statement triggers.
+	# Finds tokens with the lemma "be" in which the function is not listed in be_func
+	be_funcs = ["cop", "aux", "root", "csubj", "auxpass", "rcmod", "ccomp", "advcl", "conj","xcomp","parataxis","vmod","pcomp"]
+	if lemma == "be" and func not in be_funcs:
 		print "WARN: invalid dependency of lemma 'be' > " + func + inname
 
 	# Finds mistagged aux functions
 	if func == "aux" and lemma != "be" and lemma != "have" and lemma !="do" and pos!="MD" and pos!="TO":
 		print "WARN: aux must be modal, 'be,' 'have,' or 'do'" + inname
 
-
+	if re.search(r"n’t|n`t|[’`](s|ve|d|ll|m|re|t)", lemma, re.IGNORECASE) is not None:
+		print "WARN: non-ASCII apostrophe in lemma" + inname
