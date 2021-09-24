3fd07823e900f11ce1f8e1b2a3e4710f0f4a5d7a
amir-zeldes <amir.zeldes@georgetown.edu>
2021-09-24 15:20:04 -0400

    remove old gold constituent trees   * Tokenization no longer matches due to HYPH re-tokenization   * Refer to GUM release versions <=7.2.0 for old gold constituent trees of eight documents: "GUM_academic_discrimination","GUM_bio_emperor","GUM_fiction_oversite","GUM_interview_peres","GUM_news_nasa","GUM_reddit_polygraph","GUM_voyage_athens","GUM_whow_arrogant"

--- _build/build_gum.py
+++ _build/build_gum.py
@@ -120,9 +120,11 @@ def const_parse(gum_source, warn_slash_tokens=False, reddit=False):
 			continue
 		xmlfiles.append(file_)
 
-	# Do not overwrite gold constituent parse trees
-	gold_const = ["GUM_academic_discrimination","GUM_bio_emperor","GUM_fiction_oversite","GUM_interview_peres",
-				  "GUM_news_nasa","GUM_reddit_polygraph","GUM_voyage_athens","GUM_whow_arrogant"]
+	#gold_const = ["GUM_academic_discrimination","GUM_bio_emperor","GUM_fiction_oversite","GUM_interview_peres",
+	#			  "GUM_news_nasa","GUM_reddit_polygraph","GUM_voyage_athens","GUM_whow_arrogant"]
+	# Gold constituents must now be overwritten since they no longer correspond to HYPH tokenization.
+	# Refer to GUM version <=7.2 for gold constituent trees of these 8 documents with old tokenization
+	gold_const = []
 	xmlfiles = [f for f in xmlfiles if os.path.basename(f).replace(".xml","") not in gold_const]
 
 	for docnum, xmlfile in enumerate(xmlfiles):
