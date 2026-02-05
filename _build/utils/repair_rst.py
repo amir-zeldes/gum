d9f829e3693c141655c743af20f8514640525a36
amir-zeldes <amir-zeldes@users.noreply.github.com>
2026-02-05 17:01:25 -0500

    negative attribution warning exceptions

--- _build/utils/repair_rst.py
+++ _build/utils/repair_rst.py
@@ -80,7 +80,8 @@ def validate_rsd(rsd_line, linenum, docname):
 	if re.search(r'(\bn.t\b[^\n]+)attribution-positive_r', rsd_line) is not None:
 		if ("surprised" not in rsd_line and "not only" not in rsd_line) and "n't also deny" not in rsd_line and \
 			not ("n't think" in rsd_line and "veronique" in docname) and not ("agreeing" in rsd_line and "raven" in docname) and \
-				not ("has n't this Court" in rsd_line and "insanity" in docname) and not ("I ca n't believe" in rsd_line and "marcie" in docname):
+				not ("has n't this Court" in rsd_line and "insanity" in docname) and not ("I ca n't believe" in rsd_line and "marcie" in docname) and \
+				not "n't disagree" in rsd_line and not "did n't you" in rsd_line:
 			sys.stderr.write("! suspicious attribution-positive_r with negation" + inname)
 	if "\t" in rsd_line:
 		fields = rsd_line.split("\t")
